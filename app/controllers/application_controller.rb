140396e071440a56f5d50ee2ab430c21476cab75
Hao Wu <sys.entropy@gmail.com>
2014-10-15 01:10:21 +0000

    first commint

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,28 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  
+  def process_name
+    @name = params["name"]
+    if @name == "Hao"
+      @message = "Hey"
+    else
+      @message = "Hello"
+    end
+  end
+  
+  
+  
+  def get_city
+  end
+  
+  def process_city
+    if params["city"] == "Chicago"
+      @message = "Awesome"
+    else
+      @message = "...bumer"
+    end
+  end
+  
+end
